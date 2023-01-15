00e51ea98f8c09acdfd42acf05cb36834eb9f21e
furkankarakocmobilist <furkan.karakoc@mobilistbilisim.com>
2023-01-15 23:46:09 +0300

    Add basic timer

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    
+    var timer = Timer()
+    var counter = 0
+    
+    func startTimer() {
+        counter = 10
+        timeLabel.text = "Time: \(counter)"
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunction), userInfo: nil, repeats: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    @objc func timerFunction(){
+        timeLabel.text = "Time: \(counter)"
+        counter -= 1
+        if counter == -1 {
+            timer.invalidate()
+            timeLabel.text =  "Time is Over"
+        }
     }
 
-
+    @IBAction func startTimeButton(_ sender: Any) {
+        startTimer()
+    }
+    
 }
 
